509b301e2b5d36208aa04dc51cf10fe8b5dec317
Noel <andrew98noel@gmail.com>
2020-06-03 15:36:32 -0500

    index y login

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BIEMVENIDO A DOMINGO!!</title>
+        <link rel="shortcut icon" type="image/ico" href="assets/images/monflix.ico">
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css">
+    </head>
+    <body>
+          
+    <div class="signInContainer">
+
+<div class="column">
+
+    <div class="header">
+        <img src="assets/images/logo.png" title="Logo" alt="Site logo" />
+        <h3>INICIA SESION</h3>
+        <span>Para continuar en DOMINGO!</span>
+    </div>
+
+     <form method="POST">
+        <input type="text" name="username" placeholder="username" required>
+
+        <input type="password" name="password" placeholder="Password" required>
+
+        <input type="submit" name="submitButton" value="SUBMIT">
+        <br>
+
+
+    </form>
+
+    <a href="registrar.php" class="signInMessage">Necesitas una cuenta? Registrate aqui!</a>
+
+</div>
+
+</div>
+    </body>
+</html>
