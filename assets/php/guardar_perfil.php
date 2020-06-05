5fa889cb8a5a50f27112f10e31b7a7b2add9e8b7
Noel <andrew98noel@gmail.com>
2020-06-05 14:24:55 -0500

    agregar perfil

---
+++ assets/php/guardar_perfil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<?php 
+$variable1=" PHP 5";
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />    
+</head>
+<body>
+    <?php
+        include("conectar.php");
+        $nombre= $_POST['nombreP'];
+        $varcheck = $_POST['check'];
+       $idioma = $_POST['idioma']; 
+
+       $query= "INSERT INTO perfil (nombre,idioma, idContenido) VALUES('$nombre', '$idioma', '$varcheck')";
+       //$resultado= $conexion->query($query);S
+       if (mysqli_query($conexion, $query)) {
+           echo "New record created successfully";
+         } else {
+           echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+         }
+        
+
+        
+    ?>
+
+</body> 
+</html>
