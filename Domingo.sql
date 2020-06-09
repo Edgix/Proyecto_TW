377e479239b9b603a21a0c89e266e6a889f6ff5a
Noel <andrew98noel@gmail.com>
2020-06-08 21:04:35 -0500

    db

--- Domingo.sql
+++ Domingo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-06-2020 a las 05:27:35
+-- Tiempo de generación: 09-06-2020 a las 04:03:23
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.12
 
