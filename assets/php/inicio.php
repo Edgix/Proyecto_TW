126fc81dbb9a383666a818585f293c8a60c80eda
Edgix <31831356+Edgix@users.noreply.github.com>
2020-06-09 23:57:20 -0500

    Ya casi terminamos :c

--- assets/php/inicio.php
+++ assets/php/inicio.php
@@ -25,15 +25,18 @@
                     <li class="submenu"><a href="#" class="ne">&#9776;</a>
                         <ul class="children">
                             <?php
-                            $usua=$_SESSION["usuario"] ;
                             include("conectar.php");
-                            $query = "SELECT * FROM `perfil` ORDER BY 'idCuenta' = '$usua' ";
+                            $query = "SELECT * FROM `perfil` WHERE `idCuenta` = '33'";
                             $resultado = $conexion->query($query);
                             while ($valores = mysqli_fetch_array($resultado)) {
-                                echo '<li><a href="">' . $valores["nombre"] . ' </a></li';
+                                echo '<li class= "eperfil"> <a>' . $valores["nombre"] . '</a></li>';
                             }
                             ?>
                             <li><a href="">Agregar Perfil</a></li>
+
+
+
+
                             <li class="cerrar-sesion"><a href="../../includes/logout.php">Cerrar sesión</a></li>
                         </ul>
                     </li>
@@ -67,25 +70,7 @@
         <div class="col-md-15">
             <div class="carousel slide multi-image-slider" id="theCarousel">
                 <div class="carousel-inner">
-                    <?php
-                    $conn = mysqli_connect("localhost", "root", "root1234", "Domingo");
-                    $sqlQuery = "SELECT * FROM pelicula WHERE idClasificacion = 3 ORDER BY pelicula.idPelicula ASC";
-                    $resultSet = mysqli_query($conn, $sqlQuery);
-                    $setActive = 0;
-                    $sliderHtml = '';
-                    while ($sliderImage = mysqli_fetch_assoc($resultSet)) {
-                        $activeClass = "";
-                        if (!$setActive) {
-                            $setActive = 1;
-                            $activeClass = 'active';
-                        }
-                        $sliderHtml .= "<div class='item " . $activeClass . "'>";
-                        $sliderHtml .= "<div class='col-xs-2'>";
-                        $sliderHtml .= "<img src='../images/peliculas/" . $sliderImage['Imagen'] . "' class='img-responsive'>";
-                        $sliderHtml .= "</div></div>";
-                    }
-                    echo $sliderHtml;
-                    ?>
+                    
                 </div>
                 <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
                 <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
@@ -99,36 +84,21 @@
         <div class="col-md-15">
             <div class="carousel slide multi-image-slider" id="theCarousel2">
                 <div class="carousel-inner">
-                    <?php
-                    $conn = mysqli_connect("localhost", "root", "root1234", "Domingo");
-                    $sqlQuery = "SELECT * FROM serie WHERE idClasificacion = 4 ORDER BY serie.idSerie ASC";
-                    $resultSet = mysqli_query($conn, $sqlQuery);
-                    $setActive = 0;
-                    $sliderHtml = '';
-                    while ($sliderImage = mysqli_fetch_assoc($resultSet)) {
-                        $activeClass = "";
-                        if (!$setActive) {
-                            $setActive = 1;
-                            $activeClass = 'active';
-                        }
-                        $sliderHtml .= "<div class='item " . $activeClass . "'>";
-                        $sliderHtml .= "<div class='col-xs-2'><a href='" . $sliderImage['idSerie'] . "'>";
-                        $sliderHtml .= "<img src='../images/series/" . $sliderImage['Imagen'] . "' class='img-responsive'>";
-                        $sliderHtml .= "</a></div></div>";
-                    }
-                    echo $sliderHtml;
-                    ?>
+                    
                 </div>
                 <a class="left carousel-control" href="#theCarousel2" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
                 <a class="right carousel-control" href="#theCarousel2" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
             </div>
-
         </div>
     </div>
     <script src="../js/main.js"></script>
     <script src="../js/jquery.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/multiple-image-slider.js"></script>
+                <script>
+
+                    
+                 </script>
 </body>
 
 </html>
